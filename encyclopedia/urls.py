db7fe3eed22f5a571b8f9307f7058ae297a9ba87
Jessica-Mansell <jcmansell88@gmail.com>
2022-06-30 16:48:38 -0500

    different approach

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from django.contrib import admin
 
-import wiki
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path("wiki/<str:title>/", views.link_page, name="wiki"),
-    path("wiki/", views.SearchPage, name="wiki_search") 
+    path("wiki/<str:title>/", views.link_page, name="entries"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -19,38 +19,6 @@ def index(request):
 
 def link_page(request):
     return render(request, "encyclopedia/info_page.html", {
-        "entries":util.save_entry(filename)
+        "title" : title
     })
 
-class SearchPage(forms.Form):
-    query = forms.CharField(label="New Item",
-    hint_text="Search the Wiki")
-
-    def search_wiki(request):
-        form = SearchPage()
-        context = { 
-            "form": form
-        }
-
-        response = render(request, "encyclopedia/layout.html", context)
-        return response
-
-class EditEntryPage(forms.Form):
-    title= forms.CharField(widget=forms.HiddenInput, label="title")
-    content = forms.CharField(widget=forms.Textarea, label="content")
-
-    def edit_page(request):
-        if request.method == "POST":
-            form = SearchPage(request.POST)
-
-            if form.is_valid():
-                title = form.scrubbed_data["title"]
-                content = util.get_entry(title)
-                form = EditEntryPage(initial={
-                    "title": title,
-                    "content": content
-                })
-                return render(request, "encyclopedia/edit_page.html", {
-                    "title":title,
-                    "form": form
-                })
