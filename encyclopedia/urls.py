ead231a20ce7a6d98b32b36997db4ff02d0fe47e
Jessica-Mansell <jcmansell88@gmail.com>
2022-06-30 22:53:09 -0500

    search updated 100%

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib import admin
 
+import wiki
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path("wiki/<str:title>/", views.link_page, name="entries"),
-    path("search_page/", views.SearchForm, name="search_query")
+    path("search_page/", views.search, name="search_query")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -30,36 +30,36 @@ def link_page(request, title):
 class SearchForm(forms.Form):
     query = forms.CharField(label='New Search', max_length=50)
 
-    def search(request):
-        # populate the form
-        form = SearchForm()
-        # prepare for substring search with empty list for possible matches
-        queries_list = []
-        # check for GET request method and run request
-        if request.method == "GET":
-            form = SearchForm(request.GET)
-            # check for valid input and clean up the data to query
-            if form.is_valid():
-                # loop through list of entries
-                for entry in util.list_entries():
-                    # put query through a cleaned_data, returns as string and makes all lowercase
-                    match_same = form.cleaned_data["query"].casefold() == entry.casefold()
-                    # if substring query matches anything, clean entry as well
-                    substring_match = form.cleaned_data["query"].casefold() in entry.casefold()
-                    # query match shows user the matching page
-                    if match_same:
-                        return HttpResponseRedirect(reverse("wiki",
-                        kwargs={"title": entry}))
-                    # shows list of possible matches
-                    elif substring_match:
-                        substring_match.append(entry)
+def search(request):
+    # populate the form
+    form = SearchForm()
+    # prepare for substring search with empty list for possible matches
+    queries_list = []
+    # check for GET request method and run request
+    if request.method == "GET":
+        form = SearchForm(request.GET)
+        # check for valid input and clean up the data to query
+        if form.is_valid():
+            # loop through list of entries
+            for entry in util.list_entries():
+                # put query through a cleaned_data, returns as string and makes all lowercase
+                match_same = form.cleaned_data["query"].casefold() == entry.casefold()
+                # if substring query matches anything, clean entry as well
+                substring_match = form.cleaned_data["query"].casefold() in entry.casefold()
+                # query match shows user the matching page
+                if match_same:
+                    return HttpResponseRedirect(reverse("entries",
+                    kwargs={"title": entry}))
+                # shows list of possible matches
+                elif substring_match:
+                    substring_match.append(entry)
 
-        context = {
-            "form": SearchForm(),
-            "substring_match": substring_match
-        }
+    context = {
+        "form": SearchForm(),
+        "substring_match": substring_match
+    }
 
-        response = render(request, "encyclopedia/search_page.html", context)
-        return response
+    response = render(request, "encyclopedia/search_page.html", context)
+    return response
 
 
